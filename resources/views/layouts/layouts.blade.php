f2ac47506131c5cd1f727a3a0902516468ae89bd
ldelcaribe <laboratoriodelcaribe1@gmail.com>
2024-05-22 19:29:26 -0500

    nueva version 22052024.6

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -151,15 +151,15 @@
                                         </form>
                                     </div>
                                 @else
-                                    <div>
+                                    <div class="h-auto flex flex-col space-y-2">
                                         <a href="{{ route('login') }}"
-                                            class="rounded-md px-3 py-2 ring-1 ring-transparent transition focus:outline-none  hover:text-black/70 hover:bg-gray-200 focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                            class="rounded-md px-3 py-1 ring-1 ring-transparent transition focus:outline-none  hover:text-black/70 hover:bg-gray-200 focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                             Log in
                                         </a>
 
                                         @if (Route::has('register'))
                                             <a href="{{ route('register') }}"
-                                                class="rounded-md px-3 py-2 ring-1 ring-transparent transition  hover:text-black/70 hover:bg-gray-200 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                                class="rounded-md px-3 py-1 ring-1 ring-transparent transition  hover:text-black/70 hover:bg-gray-200 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                                 Registrese
                                             </a>
                                         @endif
